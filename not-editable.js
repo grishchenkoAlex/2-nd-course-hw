be9a4c6722128f851c72370c9343490077ac0657
Alex Grishcchenko <leh.777@mail.ru>
2023-08-07 21:37:37 +0300

    completing lesson 8

--- not-editable.js
+++ not-editable.js
@@ -1,46 +1,8 @@
+import { getListStudentsNotEdit } from "./listStudents.js";
+// Т. к. renderStudents экспортировалась по умолчанию default,
+// то имя функции мы не берем в фигурные скобки
+import renderStudents from "./renderStudents.js";
+
 const listElement = document.getElementById("list");
 
-const students = [
-    {
-        name: "Глеб",
-        color: "#ff2600",
-    },
-];
-
-const renderStudents = () => {
-    const studentsHtml = students
-        .map((student, index) => {
-            return `
-      <li class="student" data-color="${student.color}">
-        <p class="student-name">
-          ${student.name}
-        </p>
-        <p>Любимый цвет: ${student.color} <span style="
-          display:inline-block;
-          width: 15px;
-          height: 15px;
-          background-color: ${student.color};">
-        </span></p>
-      </li>`;
-        })
-        .join("");
-
-    listElement.innerHTML = studentsHtml;
-
-    const deleteButtons = document.querySelectorAll(".delete-button");
-
-
-    for (const deleteButton of deleteButtons) {
-        deleteButton.addEventListener("click", (event) => {
-            event.stopPropagation();
-
-            const index = deleteButton.dataset.index;
-
-
-            students.splice(index, 1);
-            renderStudents();
-        });
-    }
-};
-
-renderStudents();
+renderStudents(listElement, getListStudentsNotEdit);
